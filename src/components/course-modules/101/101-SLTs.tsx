49ecef6321f10e06b4e940dbbf922ab992f9710e
jamesdunseith <jdunseith@gimbalabs.io>
2023-03-07 10:43:06 -0500

    Add new SLT view to Module 101

--- src/components/course-modules/101/101-SLTs.tsx
+++ src/components/course-modules/101/101-SLTs.tsx
@@ -2,15 +2,9 @@ import SLTsItems from "@/src/components/course-modules/SLTs";
 import React from "react";
 
 const SLTs101 = () => {
-  const items = [
-    { id: "101.1", SLT: "Puedo compilar un validador plutus a Untyped Plutus Core (UPLC)." },
-    { id: "101.2", SLT: "Entiendo el rol que juega el UPLC en una aplicación decentralizada en Cardano." },
-    { id: "101.3", SLT: "Puedo utilizar Demeter.run para compilar un script en formato .plutus" },
-    { id: "101.4", SLT: "Puedo configurar un ambiente local para desarrollar Plutus utilizando un nix shell, y utilizarlo para compilar un script en formato .plutus"},
-  ];
 
   return (
-    <SLTsItems module="Módulo 101" items={items} />
+    <SLTsItems moduleTitle="Module 101" moduleNumber={101} />
   );
 };
 
